a712c586e5b9da0a5b43cd0da54f62f00a2f53ae
ghayyurali-tx <ghayyurali@techcreatix.com>
2019-07-15 18:32:10 +0500

    modified production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -87,6 +87,22 @@ Rails.application.configure do
   end
 
   # Do not dump schema after migrations.
+  config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { :host => 'lvh.me:3000' }
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default :charset => "utf-8"
+  config.action_mailer.smtp_settings = {
+      :address => "smtp.gmail.com",
+      :port => 587,
+      :domain => 'localhost:3000',
+      :user_name => ENV["GMAIL_USERNAME"],
+      :password => ENV["GMAIL_PASSWORD"],
+      :authentication => :plain,
+      :enable_starttls_auto => true
+  }
 
 
 end
