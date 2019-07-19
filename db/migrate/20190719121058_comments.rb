834ae09b6ab654f29b543c32bd0a7bda1db41a1f
ghayyurali-tx <ghayyurali@techcreatix.com>
2019-07-19 22:15:02 +0500

    fixing migration

--- db/migrate/20190719121058_comments.rb
+++
@@ -1,5 +0,0 @@
-class Comments < ActiveRecord::Migration[5.1]
-  def change
-    add_foreign_key :users, :comments
-  end
-end
--- db/migrate/20190719133829_new.rb
+++
@@ -1,6 +0,0 @@
-class New < ActiveRecord::Migration[5.1]
-  def change
-
-    add_foreign_key :comments, :users
-  end
-end
