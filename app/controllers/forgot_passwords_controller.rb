7e637df63289fc6ea35ee6512f8b95ec63d47d36
ghayyurali-tx <ghayyurali@techcreatix.com>
2019-08-07 22:40:30 +0500

    done forgot password

--- app/controllers/forgot_passwords_controller.rb
+++ app/controllers/forgot_passwords_controller.rb
@@ -8,9 +8,10 @@ class ForgotPasswordsController < ApplicationController
     if user
       user.update_with_token!
       UserMailer.send_forgot_password(user).deliver
-      redirect_to forgot_password_confirmation_path
+      flash[:alert] = "Check Your Email for Password Reset Instructions."
+      redirect_to login_path
     else
-      flash[:error] = params[:email].blank? ? "Email cannot be blank." : "There is no user this email address in the system."
+      flash[:alert] = params[:email].blank? ? "Email cannot be blank." : "There is no user with this email address in the system."
       redirect_to forgot_passwords_path
     end
   end
--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -1,25 +1,35 @@
 class PasswordResetsController < ApplicationController
-
+layout 'for_login'
   def show
-    user = User.find_by(token: params[:id])
-    if user
-      @token = user.token
+    @user = User.find_by(token: params[:id])
+    @token = @user.token
+    if @user
+      @token = @user.token
     else
-      redirect_to expired_token_path unless user
+      flash[:alert] = "Your reset password link has expired."
+      redirect_to forgot_passwords_path unless @user
     end
+    # render :show ,layout: 'for_login'
   end
 
   def create
-    user = User.find_by(token: params[:token])
-    if user && user.update(password: params[:password], token: nil)
-      flash[:success] = "Your password has been changed. Please sign in."
+     @user = User.find_by(token: params[:token])
+     @token = @user.token
+     # render layout: 'for_login'
+    if @user && @user.update(password: params[:password],password_confirmation: params[:password_confirmation], token: nil)
+      flash[:alert] = "Your password has been changed. Please sign in."
       redirect_to login_path
-    elsif user
-      @token = user.token
-      render :show
-    else
-      redirect_to expired_token_path
+      else
+    # if @user
+    #   @token = @user.token
+    #   render 'show' ,layout: 'for_login'
+    redirect_to password_reset_path(@token)
+    # else
+    #   # redirect_to expired_token_path
+    #   flash[:alert] = "Your reset password link has expired."
+    #    redirect_to forgot_passwords_path
+    # end
     end
-  end
+    end
 
 end
