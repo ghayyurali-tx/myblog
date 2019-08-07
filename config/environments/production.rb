57b01101a03cc18a76ad97ea2f7636333d192264
ghayyurali-tx <ghayyurali@techcreatix.com>
2019-08-07 13:47:55 +0500

    updated email host for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,7 +92,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
 
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { :host => 'arcane-shore-87345.herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => 'gforgeeks.herokuapp.com' }
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default :charset => "utf-8"
