74c0cf125939e7fdc285b47d81f23f533ed0cd2c
ghayyurali-tx <ghayyurali@techcreatix.com>
2019-08-09 14:49:32 +0500

    updated email for heroku

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -8,6 +8,7 @@ class UserMailer < ApplicationMailer
 
   def send_forgot_password(user)
     @user = user
-    mail to: user.email, from: "info@myflix.com", subject: "Please reset your password"
+    mail(:to => "#{user.name} <#{user.email}>", :subject => "Please reset your password")
+    # mail to: user.email, from: "info@myflix.com", subject: "Please reset your password"
   end
 end
